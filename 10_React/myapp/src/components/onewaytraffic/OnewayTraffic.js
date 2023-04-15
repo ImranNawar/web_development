728223b0477ce246814ace52bdc989b6a98bc155
ImranNawar <imran1nawar@gmail.com>
2023-04-15 09:12:27 +0500

    One way traffic concept

---
+++ 10_React/myapp/src/components/onewaytraffic/OnewayTraffic.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Display from './Display'
+
+export default class OnewayTraffic extends Component {
+  render() {
+    return (
+      <div>
+        <Display />
+      </div>
+    )
+  }
+}
