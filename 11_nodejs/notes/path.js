6ef384740675593efe002240f07267017a1ea0e1
ImranNawar <imran1nawar@gmail.com>
2023-09-03 09:15:59 +0500

    Rename the parent folder

--- 11_nodejs/notes/path.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-// To get absolute path
-const s = path.resolve("data.json");
-console.log(s);
-
-// To give you last folder
-const d = path.basename("D:\\web_development\\11_nodejs\\notes")
-console.log(d);
