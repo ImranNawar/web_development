ccc999e5c7e6fb88516d09452742e04a1e89859e
ImranNawar <imran1nawar@gmail.com>
2023-10-09 08:10:14 +0500

    set according to convention

--- 10_React/myapp/src/App.js
+++ 10_React/myapp/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+// import Home from './components/Home'
+// import Card from './components/Card';
+// import Card1 from './components/Card1';
+// import App from './components/nestedComponent/App';
+// import Counter from './components/Counter';
+// import ShowHide from './components/ShowHide'
+// import Lists from './components/Lists'
+// import LifeCycle from './components/LifeCycle';
+// import Events from './components/Events';
+// import Forms from './components/Forms'
+// import OnewayTraffic from './components/onewaytraffic/OnewayTraffic';
+// import App from './fcomponents/App'
+// import App from './routes/App'
+// import Login from './form/Login';
+// import FormikLogin from './form/FormikLogin';
+import Ref from './hooks/Ref'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {/* <Card
+      image="https://randomuser.me/portraits/men/51.jpg"
+      name="Jan"
+      joined="Joined in 2021"
+      desc="He is a student"
+      noOfFriends="4"
+    />
+    <Card
+      image="https://randomuser.me/portraits/men/52.jpg"
+      name="Khan"
+      joined="Joined in 2022"
+      desc="He is an aritist"
+      noOfFriends="1"
+    /> */}
+
+    {/* <Card1
+      title="This is title form outside the class"
+      buttonText="CLicked me"
+    /> */}
+
+    {/* <Counter /> */}
+    {/* <ShowHide /> */}
+    {/* <Lists /> */}
+    {/* <LifeCycle /> */}
+    {/* <Events /> */}
+    {/* <Forms /> */}
+    {/* <OnewayTraffic /> */}
+    {/* <App /> */}
+    {/* <App /> */}
+    {/* <Login /> */}
+    {/* <FormikLogin /> */}
+    <Ref />
     </div>
   );
 }
--- 10_React/myapp/src/index.js
+++ 10_React/myapp/src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css'
-// import App from './App';
-// import Home from './components/Home'
-// import Card from './components/Card';
-// import Card1 from './components/Card1';
-// import App from './components/nestedComponent/App';
-// import Counter from './components/Counter';
-// import ShowHide from './components/ShowHide'
-// import Lists from './components/Lists'
-// import LifeCycle from './components/LifeCycle';
-// import Events from './components/Events';
-// import Forms from './components/Forms'
-// import OnewayTraffic from './components/onewaytraffic/OnewayTraffic';
-// import App from './fcomponents/App'
-// import App from './routes/App'
-// import Login from './form/Login';
-// import FormikLogin from './form/FormikLogin';
-import Ref from './hooks/Ref'
+
+
 
 
 import reportWebVitals from './reportWebVitals';
@@ -26,39 +12,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Card
-      image="https://randomuser.me/portraits/men/51.jpg"
-      name="Jan"
-      joined="Joined in 2021"
-      desc="He is a student"
-      noOfFriends="4"
-    />
-    <Card
-      image="https://randomuser.me/portraits/men/52.jpg"
-      name="Khan"
-      joined="Joined in 2022"
-      desc="He is an aritist"
-      noOfFriends="1"
-    /> */}
-
-    {/* <Card1
-      title="This is title form outside the class"
-      buttonText="CLicked me"
-    /> */}
-
-    {/* <App /> */}
-    {/* <Counter /> */}
-    {/* <ShowHide /> */}
-    {/* <Lists /> */}
-    {/* <LifeCycle /> */}
-    {/* <Events /> */}
-    {/* <Forms /> */}
-    {/* <OnewayTraffic /> */}
-    {/* <App /> */}
-    {/* <App /> */}
-    {/* <Login /> */}
-    {/* <FormikLogin /> */}
-    <Ref />
+    <App />
   </React.StrictMode>
 );
 
